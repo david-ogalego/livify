b42e90bb16bec6bd67947e002ae9ec97c07e7fa3
david.gomez <david.gomez@beezy.net>
2019-09-29 12:06:59 +0200

    Remove material-ui from List and add some styles

--- components/SummariesList/List.js
+++ components/SummariesList/List.js
@@ -1,33 +1,59 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
 import Card from '../../components/Card';
-import { Box, Container } from '@material-ui/core';
-
-const useStyles = makeStyles(theme => ({
-    root: {
-        marginTop: 10,
-        flexGrow: 1
-    }
-}));
 
 export default ({ summaries }) => {
-    const classes = useStyles();
     return (
-        <Container className={classes.root}>
-            <Box>
-                <Grid container spacing={2}>
-                    <Grid item xs={12}>
-                        <Grid container justify="center" spacing={2}>
-                            {summaries.map((summary, index) => (
-                                <Grid xs={3} key={index} item>
-                                    <Card summary={summary} />
-                                </Grid>
-                            ))}
-                        </Grid>
-                    </Grid>
-                </Grid>
-            </Box>
-        </Container>
+        <React.Fragment>
+            <div className="containerRoot">
+                <ul className="list">
+                    {summaries.map((summary, index) => (
+                        <li className="list-item" key={index}>
+                            <Card summary={summary} />
+                        </li>
+                    ))}
+                </ul>
+            </div>
+            <style jsx>{`
+                .containerRoot {
+                    width: 100%;
+                    box-sizing: border-box;
+                    margin-left: auto;
+                    margin-right: auto;
+                    margin-top: 10px;
+                }
+
+                .list {
+                    width: calc(100% + 16px);
+                    margin: -8px;
+                    justify-content: center;
+                    display: flex;
+                    flex-wrap: wrap;
+                    box-sizing: border-box;
+                    list-style-type: none;
+                    padding: 0;
+                }
+
+                .list-item {
+                    flex-grow: 0;
+                    max-width: 24%;
+                    flex-basis: 24%;
+                    padding: 5px;
+                }
+
+                @media (min-width: 1280px) {
+                    .containerRoot {
+                        max-width: 1280px;
+                    }
+                }
+
+                @media (min-width: 960px) {
+                    .containerRoot {
+                        padding-left: 32px;
+                        padding-right: 32px;
+                    }
+                }
+            `}</style>
+        </React.Fragment>
     );
 };
