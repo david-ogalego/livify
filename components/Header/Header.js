63e8a897fc8d249bafceb375e7f38feefca9ff23
david.gomez <david.gomez@beezy.net>
2019-09-29 11:35:58 +0200

    Create header without material-ui

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -1,10 +1,42 @@
 import React from 'react';
-import { AppBar, Toolbar, Typography } from '@material-ui/core';
 
 export default () => (
-    <AppBar color="primary" position="sticky">
-        <Toolbar>
-            <Typography variant="h6">Livify</Typography>
-        </Toolbar>
-    </AppBar>
+    <React.Fragment>
+        <header>
+            <div className="container">
+                <h6>Livify</h6>
+            </div>
+        </header>
+        <style jsx>{`
+            header {
+                top: 0;
+                left: auto;
+                right: 0;
+                position: sticky;
+                box-shadow: 0px 2px 4px -1px rgba(0, 0, 0, 0.2),
+                    0px 4px 5px 0px rgba(0, 0, 0, 0.14),
+                    0px 1px 10px 0px rgba(0, 0, 0, 0.12);
+                color: #fff;
+                background-color: #3f51b5;
+            }
+
+            .container {
+                min-height: 64px;
+                padding-left: 24px;
+                padding-right: 24px;
+                display: flex;
+                position: relative;
+                align-items: center;
+            }
+
+            h6 {
+                font-size: 1.25rem;
+                font-family: 'Roboto', 'Helvetica', 'Arial', sans-serif;
+                font-weight: 500;
+                line-height: 1.6;
+                letter-spacing: 0.0075em;
+                margin: 0;
+            }
+        `}</style>
+    </React.Fragment>
 );
