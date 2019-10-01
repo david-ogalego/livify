befac0bb958417890413d65d3bf8c263ee8a522c
david.gomez <david.gomez@beezy.net>
2019-10-01 19:43:50 +0200

    Add ids to summary and videos

--- repositories/SummaryRepository.js
+++ repositories/SummaryRepository.js
@@ -1,9 +1,33 @@
 import fetch from 'isomorphic-unfetch';
 
 export default class SummaryRepository {
-    fetchSummaries() {
-        return fetch('https://www.scorebat.com/video-api/v1/').then(res =>
-            res.json()
-        );
+    async fetchSummaries() {
+        return new Promise(async (resolve, reject) => {
+            try {
+                const summaries = await fetch(
+                    'https://www.scorebat.com/video-api/v1/'
+                );
+                const summariesJson = await summaries.json();
+                const summariesResult = summariesJson.map((summary, index) => {
+                    const newVideos =
+                        summary.videos && summary.videos.length
+                            ? summary.videos.map((video, index) => {
+                                  return {
+                                      id: index,
+                                      ...video
+                                  };
+                              })
+                            : [];
+                    return {
+                        id: index,
+                        ...summary,
+                        videos: newVideos
+                    };
+                });
+                resolve(summariesResult);
+            } catch (e) {
+                reject(`Error getting highlights - ${e.message}`);
+            }
+        });
     }
 }
