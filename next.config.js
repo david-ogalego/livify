f27cf1f666d35e51d595334077fa0c6ea00e58bc
david.gomez <david.gomez@beezy.net>
2019-10-05 10:25:00 +0200

    Set graphql endpoint in next config

--- next.config.js
+++ next.config.js
@@ -2,5 +2,8 @@ module.exports = {
     webpack(config) {
         config.node = { fs: 'empty' };
         return config;
+    },
+    env: {
+        GRAPHQL_ENDPOINT: 'http://localhost:3000/api/graphql'
     }
 };
