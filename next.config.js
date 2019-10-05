3a09f1224352283ca6067caff14984305cd862dd
david.gomez <david.gomez@beezy.net>
2019-10-05 12:03:57 +0200

    Add GRAPHQL_ENDPOINT depend on the environment

--- next.config.js
+++ next.config.js
@@ -1,9 +1,13 @@
+const prod = process.env.NODE_ENV === 'production';
+
 module.exports = {
     webpack(config) {
         config.node = { fs: 'empty' };
         return config;
     },
     env: {
-        GRAPHQL_ENDPOINT: 'http://localhost:3000/api/graphql'
+        GRAPHQL_ENDPOINT: prod
+            ? 'https://livify.davidgomezrequeijo.now.sh/api/graphql'
+            : 'http://localhost:3000/api/graphql'
     }
 };
