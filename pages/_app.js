a9ae92a05b8a79a4ab6878987baf7f0bdd1a3122
david.gomez <david.gomez@beezy.net>
2019-11-30 11:46:20 +0100

    Change default font

--- pages/_app.js
+++ pages/_app.js
@@ -50,7 +50,7 @@ export default class MyApp extends App {
                         color: rgba(0, 0, 0, 0.87);
                         margin: 0;
                         font-size: 0.875rem;
-                        font-family: 'Roboto', 'Helvetica', 'Arial', sans-serif;
+                        font-family: 'Muli', 'Helvetica', 'Arial', sans-serif;
                         font-weight: 400;
                         line-height: 1.43;
                         letter-spacing: 0.01071em;
